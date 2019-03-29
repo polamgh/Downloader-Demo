661a3c89776698df39b828e028ab6cd0de97d26d
Ali Ghanavati <aliali3684@gmail.com>
2019-03-29 21:03:23 +0430

    Initial Commit

---
+++ Downloader-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Downloader-Demo
+//
+//  Created by Ali Ghanavati on 1/9/1398 AP.
+//  Copyright © 1398 AP Ali Ghanavati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Downloader-DemoTests/Downloader_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Downloader_DemoTests.swift
+//  Downloader-DemoTests
+//
+//  Created by Ali Ghanavati on 1/9/1398 AP.
+//  Copyright © 1398 AP Ali Ghanavati. All rights reserved.
+//
+
+import XCTest
+@testable import Downloader_Demo
+
+class Downloader_DemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
